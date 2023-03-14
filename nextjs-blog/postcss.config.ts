a179d945e0e97d5bce68c7bd9f7cd1f461d795d8
kdh3543 <https://github.com/kdh3543>
2023-03-14 19:00:53 +0900

    next js 기본 개념 study

---
+++ nextjs-blog/postcss.config.ts
@@ -0,0 +1,6 @@
+export const modules = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
---
+++ nextjs-blog/tailwind.config.ts
@@ -0,0 +1,8 @@
+export const modules = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // For the best performance and to avoid false positives,
+    // be as specific as possible with your content configuration.
+  ],
+};
